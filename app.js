71499ddba65c82689bf0d1fa282b6dab7689c41f
Roy Sanfilipo <roysanfilipo@gmail.com>
2020-05-17 23:13:45 -0400

    carousel and api function properly

--- app.js
+++ app.js
@@ -2,19 +2,58 @@ $(() => {
 
   // Rupaul carousel images and buttons
 
-  
+// image counter
+  let currentImage = 0;
+
+  const numOfImages = $('.carousel-images').children().length -1;
+
+  // make the next button work
+
+  $('.next').on('click', () => {
+    $('.carousel-images').children().eq(currentImage).css('display', 'none')
+    if (currentImage < numOfImages) {
+      currentImage ++;
+    } else {
+      currentImage = 0;
+    }
+    $('.carousel-images').children().eq(currentImage).css('display', 'block')
+  })
+
+  // make the previous button work
+
+  $('.previous').on('click', () => {
+      $('.carousel-images').children().eq(currentImage).css('display', 'none')
+      if(currentImage > 0) {
+        currentImage--;
+      } else {
+        currentImage = numOfImages;
+      }
+      $('.carousel-images').children().eq(currentImage).css('display', 'block')
+    })
+
+
+
 
   // Generating dragqueen based on inputting a number  between 0 and 139
+
+  // when you click submit on the form
 $('form').on('submit', (event) => {
 
+// prevent the page from re-loading
     event.preventDefault();
 
+// declaring userInput and setting it to a numerical value
     const userInput = $('input[type="number"]').val();
 
+
+// pulling from the Rupaul's dragrace API link
     $.ajax(
       {
         url:'http://www.nokeynoshade.party/api/queens/all'
       }
+
+// pulling a drag queen's name, image, and quote from the Rupaul API when a user subits the
+// index number in the array of dragqueen objects
 
     ).then(
         (data) => {
